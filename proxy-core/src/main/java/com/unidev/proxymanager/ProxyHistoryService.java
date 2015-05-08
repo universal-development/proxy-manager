bcad829cccf6bcc4c7ece7c8deaee3cc1501fc03
Denis O <denis@universal-development.com>
2015-05-08 13:23:05 +0300

    default spring beans autowired

--- proxy-core/src/main/java/com/unidev/proxymanager/ProxyHistoryService.java
+++ proxy-core/src/main/java/com/unidev/proxymanager/ProxyHistoryService.java
@@ -1,12 +1,22 @@
 package com.unidev.proxymanager;
 
+import com.unidev.proxymanager.data.ProxyHistoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 /**
- * Proxy management webapp
+ * Proxy history service
  */
 @Service
 public class ProxyHistoryService {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Autowired
+    private ProxyHistoryRepository proxyHistoryRepository;
+
 
 
 }
