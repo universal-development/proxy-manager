99fbdfdde04078b4f6d2bf4a98a5cc3b7959c672
Denis O <denis@universal-development.com>
2015-05-14 13:48:48 +0300

    proxy  persist

--- proxy-core/src/main/java/com/unidev/proxymanager/ProxyHistoryService.java
+++ proxy-core/src/main/java/com/unidev/proxymanager/ProxyHistoryService.java
@@ -1,9 +1,12 @@
 package com.unidev.proxymanager;
 
+import com.unidev.proxymanager.data.ProxyHistory;
 import com.unidev.proxymanager.data.ProxyHistoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 /**
  * Proxy history service
@@ -17,6 +20,21 @@ public class ProxyHistoryService {
     @Autowired
     private ProxyHistoryRepository proxyHistoryRepository;
 
+    /**
+     * Add new proxy for history tracking
+     * @param ip
+     * @param port
+     */
+    public void addProxy(String ip, Integer port) {
+
+        ProxyHistory proxyHistory = new ProxyHistory();
+        proxyHistory.setIp(ip);
+        proxyHistory.setPort(port);
+        proxyHistory.setAddDate(new Date());
+
+        proxyHistoryRepository.save(proxyHistory);
+
+    }
 
 
 }
