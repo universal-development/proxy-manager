71bd252eac40f292af14f8a2d0ae1efdd5d0528e
Denis O <denis@universal-development.com>
2015-05-17 11:31:33 +0300

    crud test class

---
+++ proxy-core/src/test/java/com/unidev/proxymanager/integration/ProxyCrudTest.java
@@ -0,0 +1,39 @@
+package com.unidev.proxymanager.integration;
+
+
+import com.unidev.proxymanager.ProxyHistoryService;
+import com.unidev.proxymanager.data.ProxyHistory;
+import com.unidev.proxymanager.data.ProxyHistoryRepository;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/platform-common-beans.xml","/proxy-core-conf.xml"})
+@Ignore
+public class ProxyCrudTest {
+
+    private static Logger LOG = LoggerFactory.getLogger(ProxyCrudTest.class);
+
+
+    @Autowired
+    private ProxyHistoryRepository proxyHistoryRepository;
+
+    @Autowired
+    private ProxyHistoryService proxyHistoryService;
+
+    @Test
+    public void testProxyAdding() {
+
+        proxyHistoryService.addProxy("192.168.0.50", 9000);
+
+        Iterable<ProxyHistory> iterable = proxyHistoryRepository.findAll();
+        LOG.debug("Iterable {}", iterable);
+    }
+
+}
