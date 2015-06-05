455123ad5681dbfa4fefc683c9311e18412be850
Denis O <denis@universal-development.com>
2015-06-05 13:26:19 +0300

    templates for job checking  job and job factory

---
+++ proxy-core/src/main/java/com/unidev/proxymanager/executor/ProxyCheckerJob.java
@@ -0,0 +1,15 @@
+package com.unidev.proxymanager.executor;
+
+import com.unidev.platform.jobs.AbstractCallable;
+import com.unidev.platform.jobs.ExecutionResult;
+import com.unidev.proxymanager.domain.ProxyCheckResult;
+
+/**
+ * Job for proxy checking
+ */
+public class ProxyCheckerJob implements AbstractCallable {
+    @Override
+    public ExecutionResult<ProxyCheckResult> call() throws Exception {
+        return null;
+    }
+}
---
+++ proxy-core/src/main/java/com/unidev/proxymanager/executor/ProxyCheckerJobFactory.java
@@ -0,0 +1,14 @@
+package com.unidev.proxymanager.executor;
+
+import com.unidev.platform.jobs.AbstractCallable;
+import com.unidev.platform.jobs.JobFactory;
+
+/**
+ * Job factory for proxy checking jobs
+ */
+public class ProxyCheckerJobFactory implements JobFactory {
+    @Override
+    public AbstractCallable newJob() {
+        return null;
+    }
+}
