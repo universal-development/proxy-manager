8c9f8367158b73c67c23cb91f5006f27005eb663
Denis O <denis@universal-development.com>
2015-06-05 13:14:55 +0300

    Update proxy history moved in history service

--- proxy-core/src/main/java/com/unidev/proxymanager/ProxyHistoryService.java
+++ proxy-core/src/main/java/com/unidev/proxymanager/ProxyHistoryService.java
@@ -3,6 +3,7 @@ package com.unidev.proxymanager;
 import com.google.common.collect.Lists;
 import com.unidev.proxymanager.data.ProxyHistory;
 import com.unidev.proxymanager.data.ProxyHistoryRepository;
+import com.unidev.proxymanager.domain.ProxyCheckResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,10 @@ import java.util.List;
  */
 @Service
 public class ProxyHistoryService {
+
+    public static final int MAX_SCORE = 10;
+    private static final int MIN_SCORE = -10;
+
 
     @Autowired
     private ProxyHistoryRepository proxyHistoryRepository;
@@ -54,6 +59,37 @@ public class ProxyHistoryService {
     public Collection<ProxyHistory> listProxies() {
         Iterable<ProxyHistory> proxyHistory = proxyHistoryRepository.findAll();
         return Lists.newArrayList(proxyHistory);
+    }
+
+    /**
+     * Update proxy history
+     * @param proxyCheckResult Proxy check result
+     */
+    public void updateProxyHistory(ProxyHistory proxyHistory, ProxyCheckResult proxyCheckResult) {
+
+        proxyCheckResult.setCheckStatus(proxyCheckResult.getCheckStatus());
+        proxyHistory.setLastUpdate(new Date());
+        proxyHistory.setRequestTime(proxyCheckResult.getRequestTime() / 1000D);
+        proxyHistory.addRequestCount(1);
+        proxyHistory.setRemoteIp(proxyCheckResult.getRemoteIp());
+
+        if (proxyCheckResult.getCheckStatus() == ProxyCheckResult.CheckStatus.OK) {
+            proxyHistory.addScore(1);
+        }
+
+        if (proxyCheckResult.getCheckStatus() == ProxyCheckResult.CheckStatus.FAILED) {
+            proxyHistory.addScore(-1);
+        }
+
+        if (proxyHistory.getScore() > MAX_SCORE) {
+            proxyHistory.setScore(MAX_SCORE);
+        }
+
+        if (proxyHistory.getScore() < MIN_SCORE) {
+            proxyHistory.setScore(MIN_SCORE);
+        }
+
+        proxyHistoryRepository.save(proxyHistory);
     }
 
 
