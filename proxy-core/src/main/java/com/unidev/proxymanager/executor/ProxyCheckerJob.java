eeed05eb7a94d631677a82615366fc06194be85f
Denis O <denis@universal-development.com>
2015-06-06 09:33:24 +0300

    bean instantiation through spring context

--- proxy-core/src/main/java/com/unidev/proxymanager/executor/ProxyCheckerJob.java
+++ proxy-core/src/main/java/com/unidev/proxymanager/executor/ProxyCheckerJob.java
@@ -1,8 +1,15 @@
 package com.unidev.proxymanager.executor;
 
+import com.unidev.platform.ApplicationContextProvider;
 import com.unidev.platform.jobs.AbstractCallable;
 import com.unidev.platform.jobs.ExecutionResult;
+import com.unidev.platform.utils.RandomUtils;
+import com.unidev.proxymanager.ProxyChecker;
+import com.unidev.proxymanager.ProxyHistoryService;
+import com.unidev.proxymanager.data.ProxyHistory;
 import com.unidev.proxymanager.domain.ProxyCheckResult;
+
+import java.util.Collection;
 
 /**
  * Job for proxy checking
@@ -10,16 +17,18 @@ import com.unidev.proxymanager.domain.ProxyCheckResult;
 public class ProxyCheckerJob implements AbstractCallable {
     @Override
     public ExecutionResult<ProxyCheckResult> call() throws Exception {
+        ProxyHistoryService proxyHistoryService = ApplicationContextProvider.getInstance().getBean(ProxyHistoryService.class);
+        Collection<ProxyHistory> proxyList = proxyHistoryService.listProxies();
+        if (proxyList == null || proxyList.isEmpty()) {
+            return null;
+        }
+        RandomUtils randomUtils = ApplicationContextProvider.getInstance().getBean(RandomUtils.class);
+        ProxyHistory proxyHistory = randomUtils.randomValue(proxyList);
 
+        ProxyChecker proxyChecker = ApplicationContextProvider.getInstance().getBean(ProxyChecker.class);
 
-
-/*        String ip = proxyHistory.getIp();
+        String ip = proxyHistory.getIp();
         Integer port = proxyHistory.getPort();
-
-        ProxyCheckResult proxyCheckResult = proxyChecker.checkProxyStatus(ip, port);
-*/
-
-
-        return null;
+        return new ExecutionResult<ProxyCheckResult>(proxyChecker.checkProxyStatus(ip, port));
     }
 }
