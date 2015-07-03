745947d5c0fdcad451cc5373e8ef8b5fade0775c
Denis O <denis@universal-development.com>
2015-07-03 15:28:12 +0300

    Moving to spring boot & template app

--- proxy-core/src/main/java/com/unidev/proxymanager/data/ProxyHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.unidev.proxymanager.data;
-
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProxyHistoryRepository extends ElasticsearchRepository<ProxyHistory, String> {
-}
